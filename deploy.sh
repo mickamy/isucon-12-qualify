38f4c2ca9aa0c032f00e48467ad28f954573bf04
mickamy <tetsuro@mickamy.com>
2024-10-06 16:37:15 +0900

    chore: add build

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 git fetch origin --prune
 git pull origin
 
+cd go && make isuports && cd -
+
 sudo systemctl daemon-reload
 sudo systemctl restart mysql.service
 sudo systemctl restart nginx.service
